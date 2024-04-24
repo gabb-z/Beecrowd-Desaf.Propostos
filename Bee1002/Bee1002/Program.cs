ff41e68821ce101c62acd8cae067ec8ddf8c575e
gabb-z <gabbz.liima@gmail.com>
2024-04-23 21:08:58 -0300

    Desafio 03 - Área do Círculo

---
+++ Bee1002/Bee1002/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Globalization;
+
+double n = 3.14159;
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double area = n * Math.Pow(raio, 2);
+
+Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
